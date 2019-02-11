a1082468a989766bbd578036180dba118fe0d00e
Bartlomiej.Kwiatek <bartlomiej.kwiatek@vml.com>
2019-02-11 15:15:25 +0100

    status page layout update.

--- web/index.php
+++ web/index.php
@@ -42,27 +42,44 @@ require_once '../app/app.php';
         <main role="main" class="flex-shrink-0">
             <div class="container">
                 <?php if ($page === 'status' && !empty($encrypted) && !empty($decrypted) && !empty($secret)): ?>
-                    <div class="alert alert-success">
-                        <h5 class="alert-heading">
-                            <em class="fas fa-fw fa-key"></em> Secret key
-                        </h5>
-                        <p>
-                            <?php echo $secret ?>
-                        </p>
-                        <h5 class="alert-heading">
-                            <em class="fas fa-fw fa-eye"></em> Decrypted string
-                        </h5>
-                        <p>
-                            <?php echo nl2br($decrypted) ?>
-                        </p>
-                        <h5 class="alert-heading">
-                            <em class="fas fa-fw fa-eye-slash"></em> Encrypted string
-                        </h5>
-                        <p class="d-inline-block text-truncate w-100">
-                            <a href="<?php printf($config['share_url'], $encrypted); ?>" rel="nofollow">
-                                <?php echo $encrypted ?>
-                            </a>
-                        </p>
+                    <div class="list-group">
+                        <div class="list-group-item">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-2">
+                                    <em class="fas fa-fw fa-key"></em> Secret key
+                                </h5>
+                                <div class="btn-group mb-2">
+                                    <div class="btn btn-primary btn-sm js-tooltip js-copy" data-toggle="tooltip" data-placement="left" data-copy="<?php echo $secret ?>" title="Copy secret key to clipboard">
+                                        <em class="fas fa-copy"></em>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="mb-1"><?php echo $secret ?></p>
+                        </div>
+                        <div class="list-group-item">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">
+                                    <em class="fas fa-fw fa-eye"></em> Decrypted string
+                                </h5>
+                            </div>
+                            <p class="mb-1"><?php echo nl2br($decrypted) ?></p>
+                        </div>
+                        <div class="list-group-item">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-2">
+                                    <em class="fas fa-fw fa-eye-slash"></em> Encrypted string
+                                </h5>
+                                <div class="btn-group mb-2">
+                                    <div class="btn btn-primary btn-sm js-tooltip js-copy" data-toggle="tooltip" data-placement="left" data-copy="<?php echo $encrypted ?>" title="Copy entrypted string to clipboard">
+                                        <em class="fas fa-copy"></em>
+                                    </div>
+                                    <a href="<?php printf($config['share_url'], $encrypted); ?>" rel="nofollow" class="btn btn-secondary btn-sm" data-toggle="tooltip" data-placement="left" title="Open share link with encrypted string">
+                                        <em class="fas fa-link"></em>
+                                    </a>
+                                </div>
+                            </div>
+                            <p class="d-inline-block text-truncate w-100 mb-1"><?php echo $encrypted ?></p>
+                        </div>
                     </div>
                 <?php else: ?>
                     <form method="POST" action="<?php printf($config['form_url'], $action); ?>" autocomplete="off">
@@ -144,6 +161,7 @@ require_once '../app/app.php';
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
         <script>
+            <?php if ($page === 'encrypt'): ?>
             var textarea = document.getElementById("decrypted");
             var limit = 180;
             textarea.oninput = function () {
@@ -151,6 +169,39 @@ require_once '../app/app.php';
                 console.log(textarea.scrollHeight)
                 textarea.style.height = Math.min(textarea.scrollHeight, limit) + "px";
             };
+            <?php endif; ?>
+            <?php if ($page === 'status'): ?>
+            function copyToClipboard(text, el) {
+                var copyTest = document.queryCommandSupported('copy');
+                var elOriginalText = el.attr('data-original-title');
+                if (copyTest === true) {
+                    var copyTextArea = document.createElement("textarea");
+                    copyTextArea.value = text;
+                    document.body.appendChild(copyTextArea);
+                    copyTextArea.select();
+                    try {
+                        var successful = document.execCommand('copy');
+                        var msg = successful ? 'Copied!' : 'Whoops, not copied!';
+                        el.attr('data-original-title', msg).tooltip('show');
+                    } catch (err) {
+                        console.log('Oops, unable to copy');
+                    }
+                    document.body.removeChild(copyTextArea);
+                    el.attr('data-original-title', elOriginalText);
+                } else {
+                    window.prompt("Copy to clipboard: Ctrl+C or Command+C, Enter", text);
+                }
+            }
+
+            $(document).ready(function () {
+                $('.js-tooltip').tooltip();
+                $('.js-copy').click(function () {
+                    var text = $(this).attr('data-copy');
+                    var el = $(this);
+                    copyToClipboard(text, el);
+                });
+            });
+            <?php endif; ?>
         </script>
     </body>
 </html>
